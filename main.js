3f10cc88b86d39df73c17b78853676be42ad15eb
Дарига Арапбаева <darigaarapbaeva@MacBook-Air-Dariga-2.local>
2023-03-17 22:34:25 +0600

    first commit

--- main.js
+++ main.js
@@ -19,30 +19,29 @@ document.body.appendChild (p1)
 let ul = document.createElement("ul");
 
 let li1 = document.createElement("li");
-li1.innerText = "find elements in the DOM (5 points);";
+li1.innerHTML = "find elements in the DOM (<b>5 points</b>);";
 li1.style.color = "green"
-li1.style.fontWeight = "900";
 
 
 let li2 = document.createElement("li");
-li2.innerText = "create/add/remove elements (5 points);";
+li2.innerHTML = "create/add/remove elements (<b>5 points</b>);";
 li2.style.color = "purple"
 
 
 let li3 = document.createElement("li");
-li3.innerText = "change content of the elements (5 points);";
+li3.innerHTML = "change content of the elements (<b>5 points</b>);";
 li3.style.color = "green"
 
 let li4 = document.createElement("li");
-li4.innerText = "change styles of the elements (5 points);";
+li4.innerHTML = "change styles of the elements (<b>5 points</b>);";
 li4.style.color = "purple"
 
 let li5 = document.createElement("li");
-li5.innerText = "change attributes of the elements (5 points);";
+li5.innerHTML = "change attributes of the elements (<b>5 points</b>);";
 li5.style.color = "green"
 
 let li6 = document.createElement("li");
-li6.innerText = "change classes of the elements (5 points).";
+li6.innerHTML = "change classes of the elements (<b>5 points</b>).";
 li6.style.color = "purple"
 
 
@@ -63,33 +62,33 @@ document.body.appendChild (p2)
 
 let ul2 = document.createElement("ul");
 let li7 = document.createElement("li");
-li7.innerText = "Create a new repository on Github, named lab7 (1 point).";
+li7.innerHTML = "Create a new repository on Github, named <b>lab7</b> (<b>1 point</b>).";
 li7.style.color = "green"
 
 
 
 let li8 = document.createElement("li");
-li8.innerText = "Clone this repository to your local machine and work inside it.";
+li8.innerHTML = "Clone this repository to your local machine and work inside it.";
 li8.style.color = "purple"
 
 let li9 = document.createElement("li");
-li9.innerText = "Create a new HTML file, called index.html, which has only one <h1> tag with Hello, World! message (1 point).";
+li9.innerHTML = "Create a new HTML file, called <b>index.html</b>, which has only one h1 tag with 'Hello, World!' message (<b>1 point</b>).";
 li9.style.color = "green"
 
 let li10 = document.createElement("li");
-li10.innerText = "Create a new JavaScript file, called main.js, which must contain your program (assignment) described above (1 point).";
+li10.innerHTML = "Create a new JavaScript file, called <b>main.js</b>, which must contain your program (assignment) described above (<b>1 point</b>).";
 li10.style.color = "purple"
 
 let li11 = document.createElement("li");
-li11.innerText = "Link this main.js file to your index.html file (Note: place your script at the end of the body section).";
+li11.innerHTML = "Link this <b>main.js</b> file to your <b>index.html</b> file (Note: place your script at the end of the <b>body</b> section).";
 li11.style.color = "green"
 
 let li12 = document.createElement("li");
-li12.innerText = "Write a JavaScript program in main.js to make your index.html look identical to this HTML file (5 points).";
+li12.innerHTML = "Write a JavaScript program in <b>main.js</b> to make your <b>index.html</b> look identical to this HTML file (<b>5 points</b>).";
 li12.style.color = "purple"
 
 let li13 = document.createElement("li");
-li13.innerText = "After you finish your work, submit it to the Github (2 points).";
+li13.innerHTML = "After you finish your work, submit it to the Github (<b>2 points</b>).";
 li13.style.color = "green"
 
 
